c415d18e333a07d03d0d3c0cdcfabc4213542a30
frosticecold <raulcorreia7@gmail.com>
2017-05-17 20:38:27 +0100

    Refinado Adicionado ficheiro inicial dados.txt

--- src/system/listas/ListaAtribuicao.java
+++ src/system/listas/ListaAtribuicao.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import system.candidatura.Atribuicao;
-import system.candidatura.Candidatura;
 import system.user.Fae;
 
 /**
@@ -13,33 +12,34 @@ import system.user.Fae;
  * @author Raúl Correia 1090657@isep.ipp.pt
  */
 public class ListaAtribuicao implements Iterable<Atribuicao>, Serializable {
-    
-    private List<Atribuicao> lista = new ArrayList<>();
-    
+
+    private List<Atribuicao> lista;
+
     public ListaAtribuicao() {
+        lista = new ArrayList<>();
     }
-    
+
     public Atribuicao novaAtribuicao() {
-        
+
         return new Atribuicao();
     }
-    
+
     public boolean add(Atribuicao a) {
         return lista.add(a);
     }
-    
+
     public void guardarAtribuicoes(List<Atribuicao> listaAtrib) {
         for (Atribuicao atribuicao : listaAtrib) {
             add(atribuicao);
         }
-        
+
     }
-    
+
     @Override
     public Iterator<Atribuicao> iterator() {
         return lista.iterator();
     }
-    
+
     public List<Fae> obterTodosFaesAtribuidos() {
         List<Fae> listaFae = new ArrayList<>();
         for (Atribuicao a : lista) {
@@ -47,7 +47,7 @@ public class ListaAtribuicao implements Iterable<Atribuicao>, Serializable {
         }
         return listaFae;
     }
-    
+
     public List<Atribuicao> obterAtribuicoesAssociadaAoFae(String username) {
         List<Atribuicao> listaAtrib = new ArrayList<>();
         for (Atribuicao a : lista) {
@@ -57,7 +57,7 @@ public class ListaAtribuicao implements Iterable<Atribuicao>, Serializable {
         }
         return listaAtrib;
     }
-    
+
     public boolean verificarSeFaeTemAtribuicoes(String username) {
         for (Atribuicao a : lista) {
             if (a.getFae().getUsername().equals(username)) {
@@ -66,14 +66,14 @@ public class ListaAtribuicao implements Iterable<Atribuicao>, Serializable {
         }
         return false;
     }
-    
+
     public int size() {
-        
+
         return lista.size();
     }
-    
+
     public void clear() {
         lista.clear();
     }
-    
+
 }
